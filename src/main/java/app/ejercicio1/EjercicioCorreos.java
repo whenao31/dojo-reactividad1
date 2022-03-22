a9f59f6bc47a8477455ee08d894910ca800b81b7
wil <whenao86@gmail.com>
2022-03-22 11:43:17 -0500

    Crear filtrado y conteo de correos en lista

--- src/main/java/app/ejercicio1/EjercicioCorreos.java
+++ src/main/java/app/ejercicio1/EjercicioCorreos.java
@@ -10,9 +10,34 @@ public class EjercicioCorreos {
 
     public static void main(String[] args) {
         List<Correo> listaCorreos = ListaCorreos.getListaCorreos();
-        Flux<Correo> fluxCorreos = Flux.fromIterable(listaCorreos)
-                .distinct();
+
+//        checkDistinct(listaCorreos);
+        filtroDominios(listaCorreos, "gmail");
+        filtroDominios(listaCorreos, "hotmail");
+        filtroDominios(listaCorreos, "outlook");
+
     }
 
+    public static void checkDistinct(List<Correo> listaCorreos){
+        Flux<Correo> fluxCorreosDistintos = Flux.fromIterable(listaCorreos)
+                .distinct();
+        fluxCorreosDistintos.subscribe(System.out::println);
+    }
+
+    public static void filtroDominios(List<Correo> listaCorreos, String dominio){
+        Flux<Correo> fluxFiltradoDominio = Flux.fromIterable(listaCorreos)
+                .distinct()
+                .filter(email -> getEmailDomain(email.getEmail()).equals(dominio.toLowerCase()));
+
+        System.out.println(String.format("Cantidad de correos de dominio %s:",dominio.toUpperCase()));
+        fluxFiltradoDominio.count().subscribe(System.out::println);
+        System.out.println(String.format("Correos de dominio %s:",dominio.toUpperCase()));
+        fluxFiltradoDominio.subscribe(System.out::println);
+    }
+
+    public static String getEmailDomain(String email){
+        String domainExtracted = email.substring(email.indexOf("@") + 1, email.indexOf("."));
+        return domainExtracted;
+    }
 
 }
