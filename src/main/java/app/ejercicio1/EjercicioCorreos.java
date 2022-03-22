b698b22d294ec50c1582aaf9054188a3cf08f164
wil <whenao86@gmail.com>
2022-03-22 12:13:10 -0500

    Verificar formato correo con map

--- src/main/java/app/ejercicio1/EjercicioCorreos.java
+++ src/main/java/app/ejercicio1/EjercicioCorreos.java
@@ -4,6 +4,7 @@ import app.model.Correo;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EjercicioCorreos {
@@ -12,9 +13,11 @@ public class EjercicioCorreos {
         List<Correo> listaCorreos = ListaCorreos.getListaCorreos();
 
 //        checkDistinct(listaCorreos);
-        filtroDominios(listaCorreos, "gmail");
-        filtroDominios(listaCorreos, "hotmail");
-        filtroDominios(listaCorreos, "outlook");
+//        filtroDominios(listaCorreos, "gmail");
+//        filtroDominios(listaCorreos, "hotmail");
+//        filtroDominios(listaCorreos, "outlook");
+
+        mapCheckDominio(listaCorreos);
 
     }
 
@@ -33,6 +36,16 @@ public class EjercicioCorreos {
         fluxFiltradoDominio.count().subscribe(System.out::println);
         System.out.println(String.format("Correos de dominio %s:",dominio.toUpperCase()));
         fluxFiltradoDominio.subscribe(System.out::println);
+    }
+
+    public static void mapCheckDominio(List<Correo> listaCorreos){
+        List<String> listaDominios = Arrays.asList("gmail", "hotmail", "outlook", "sofka", "aol", "yahoo");
+        Flux<String> fluxMapDominio = Flux.fromIterable(listaCorreos)
+                .distinct()
+                .map(email -> listaDominios.contains(getEmailDomain(email.getEmail())) ? email.toString() : email.toString() + " no cumple");
+
+        fluxMapDominio.subscribe(System.out::println);
+
     }
 
     public static String getEmailDomain(String email){
